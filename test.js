653dba46193d84bb51f4c7e590a279fbd23da62e
Christian Reisinger <chr_reisinger@yahoo.de>
2011-08-18 17:41:28 +0200

    added test.js file

---
+++ test.js
@@ -0,0 +1,10 @@
+var x;   // var declaration
+
+x + y; // simple binary operations
+
+x = 1; // int literals
+x = "asdf"; // string literals
+var y = 1000000000;
+while(x < y) { // while loop; binary comparison operations
+   x = x + 1; // variable assignment
+}
